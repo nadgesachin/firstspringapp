98d020aea63ef6f86c23608dd29896dee2c9b33b
pat nadge <pat@pats-MacBook-Air.local>
2023-12-02 00:45:06 +0530

    first commit

---
+++ src/main/java/err/pat/firstspringapp/FirstspringappApplication.java
@@ -0,0 +1,13 @@
+package err.pat.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/err/pat/firstspringapp/FirstspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package err.pat.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
